ef547909addc339199c2a8f81176f00f66e9c871
Gary11777 <iharushakou@gmail.com>
2024-09-29 14:08:34 +0300

    28.1 Blade and Tailwind Techniques for Your Laravel Views

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -6,15 +6,6 @@
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-{{--            <a href="#" class="bg-white/10 hover:bg-white/25 px-2--}}
-{{--                        py-1--}}
-{{--                        rounded-xl text-xs transition-colors duration-300">Tag</a>--}}
-{{--            <a href="#" class="bg-white/10 hover:bg-white/25 px-2--}}
-{{--                        py-1--}}
-{{--                        rounded-xl text-xs transition-colors duration-300">Tag</a>--}}
-{{--            <a href="#" class="bg-white/10 hover:bg-white/25 px-2--}}
-{{--                        py-1--}}
-{{--                        rounded-xl text-xs transition-colors duration-300">Tag</a>--}}
             <x-tag>Tag</x-tag>
             <x-tag>Tag</x-tag>
             <x-tag>Tag</x-tag>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis
+        .com/css2?family=Hanken+Grotesk:wght@400;500;600;700&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-black text-white">
