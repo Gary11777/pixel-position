a9ab36ccfc1cfddcc5d64fe2c4f56e5f0912a256
Gary11777 <iharushakou@gmail.com>
2024-10-13 19:18:28 +0300

    28_2 Blade and Tailwind Techniques for Your Laravel Views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,11 +8,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis
-        .com/css2?family=Hanken+Grotesk:wght@400;500;600;700&display=swap"
+        .com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
         rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b
         border-white/20">
