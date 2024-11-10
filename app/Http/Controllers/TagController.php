9768577bedea35f09f6970b611b62bbbbc544f68
Gary11777 <iharushakou@gmail.com>
2024-11-10 09:31:46 +0300

    30_1 The Everything Episode

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function __invoke()
+    {
+        // jobs for this tag
+
+        return view('results', ['jobs' => $jobs]);
+    }
+}
