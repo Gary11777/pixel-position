7c9a47e4c10653eede6ea07d2d7721901845843f
Gary11777 <iharushakou@gmail.com>
2024-11-16 12:56:23 +0300

    30_2 Last Lesson of the series

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Tag $tag)
     {
-        // jobs for this tag
-
-        return view('results', ['jobs' => $jobs]);
+        return view('results', ['jobs' => $tag->jobs]);
     }
 }
